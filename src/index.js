c3c4869be104aa3e87b9289ccdb5c1396078baf7
curtis mcallister <curtis.s.mcallister@gmail.com>
2020-11-06 14:54:14 -0800

    add redux store and token debug component to app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+
+import store from './store.js'
+
+import TokenDebug from './token_debug.js'
 
 render(
-  <h1>hello world</h1>,
+  <Provider store={ store }>
+    <h1>hello world</h1>
+    <TokenDebug/>
+  </Provider>,
   document.getElementById('app')
 )
