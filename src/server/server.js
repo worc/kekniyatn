40fb8a837e3988bc1fe9f420d0a3d72ff106ea8d
curtis mcallister <curtis.s.mcallister@gmail.com>
2020-11-06 14:47:52 -0800

    append CORS header correctly, stub out https server in comments

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,18 @@
+// import fs from 'fs'
+// import { fileURLToPath } from 'url'
+// import path, { dirname } from 'path'
+// import https from 'https'
 import express from 'express'
 import { getAccessToken } from './authorization.js'
 
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const key = fs.readFileSync(path.join(__dirname, './devkey.pem'))
+// const cert = fs.readFileSync(path.join(__dirname, './devcert.pem'))
+
 const app = express()
+// const server = https.createServer({ key, cert }, app)
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
@@ -15,6 +26,7 @@ app.get('/access_token', (req, res) => {
       expires_in: response.data.expires_in,
     }
 
+    res.append('Access-Control-Allow-Origin', '*')
     res.status(200).send(data)
   }).catch(error => {
     console.error(error)
@@ -25,3 +37,7 @@ app.get('/access_token', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on port ${ PORT }`)
 })
+
+// server.listen(PORT, () => {
+//   console.log(`Server listening on port ${ PORT }`)
+// })
