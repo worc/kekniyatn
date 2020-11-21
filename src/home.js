af5096848b5aa8348638728897ceedfcc546fbe9
curtis mcallister <curtis.s.mcallister@gmail.com>
2020-11-21 11:58:04 -0800

    move debug components to a home module

---
+++ src/home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TokenDebug from './token_debug.js'
+import ArtistDebug from './artist_debug.js'
+
+export default function () {
+  return (
+    <>
+      <h1>hello world</h1>
+      <TokenDebug/>
+      <ArtistDebug/>
+    </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import store from './store.js'
 
-import TokenDebug from './token_debug.js'
-import ArtistDebug from './artist_debug.js'
+import Home from './home.js'
+
 
 render(
   <Provider store={ store }>
-    <h1>hello world</h1>
-    <TokenDebug/>
-    <ArtistDebug/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={ Home }/>
+        {/*<Route path='/timeline' component={Timeline}/>*/}
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('app')
 )
