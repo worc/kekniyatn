e777b56cdda0d1409489d51ad9f59a341589add6
curtis mcallister <curtis.s.mcallister@gmail.com>
2020-11-21 12:59:19 -0800

    smash together a proof of concept timeline generator

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import { createGlobalStyle } from 'styled-components'
 
 import store from './store.js'
 
 import Home from './home.js'
 import Timeline from './timeline.js'
 
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+`
+
 render(
   <Provider store={ store }>
+    <GlobalStyle/>
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={ Home }/>
--- src/timeline.js
+++ src/timeline.js
@@ -1,7 +1,99 @@
 import React from 'react'
+import styled from 'styled-components'
+import {
+  addDays,
+  format,
+  isBefore,
+  isSameDay,
+  isFirstDayOfMonth,
+  startOfYear
+} from 'date-fns'
 
-export default function () {
+const Container = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  padding: 16px;
+  
+  & > * {
+    flex: 0 0 auto;
+  }
+`
+
+const YearHash = styled.div`
+  border: 1px solid #ace;
+  height: 5px;
+  width: 30px;
+  
+  &::before {
+    border: 1px solid red;
+    bottom: 12px;
+    content: "";
+    display: block;
+    left: 12px;
+    position: relative;
+    height: 30px;
+    width: 5px;
+  }
+`
+
+const MonthHash = styled.div`
+  border: 1px solid #ace;
+  height: 5px;
+  width: 30px;
+  
+  &::before {
+    border: 1px solid red;
+    bottom: 8px;
+    content: "";
+    display: block;
+    left: 12px;
+    position: relative;
+    height: 20px;
+    width: 5px;
+  }
+`
+
+const DayHash = styled.div`
+  border: 1px solid #ace;
+  height: 5px;
+  width: 30px;
+  
+  &::before {
+    border: 1px solid red;
+    bottom: 6px;
+    content: "";
+    display: block;
+    left: 12px;
+    position: relative;
+    height: 15px;
+    width: 5px;
+  }
+`
+
+const startDate = new Date(2013, 11, 1)
+const endDate = new Date(2014, 8, 18)
+
+function Timeline ({ start_date, end_date }) {
+  let workingDate = start_date
+  const components = []
+  while (isBefore(workingDate, end_date)) {
+    if (isSameDay(workingDate, startOfYear(workingDate))) {
+      components.push(<YearHash title={format(workingDate, 'y MMMM do')} />)
+    } else if (isFirstDayOfMonth(workingDate)) {
+      components.push(<MonthHash title={format(workingDate, 'y MMMM do')} />)
+    } else {
+      components.push(<DayHash title={format(workingDate, 'y MMMM do')} />)
+    }
+    workingDate = addDays(workingDate, 1)
+  }
+
+  return components
+}
+
+export default function ({ start_date, end_date }) {
   return (
-    <h2>timeline</h2>
+    <Container>
+      <Timeline start_date={ startDate } end_date={ endDate }/>
+    </Container>
   )
 }
