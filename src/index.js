d6b982fe45cf3b3d50df4eacfdd5f39da4cbe49b
curtis mcallister <curtis.s.mcallister@gmail.com>
2020-11-21 12:04:09 -0800

    add timeline stub

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 
 import store from './store.js'
 
 import Home from './home.js'
-
+import Timeline from './timeline.js'
 
 render(
   <Provider store={ store }>
     <BrowserRouter>
       <Switch>
         <Route exact path='/' component={ Home }/>
-        {/*<Route path='/timeline' component={Timeline}/>*/}
+        <Route path='/timeline' component={Timeline}/>
       </Switch>
+      <footer>
+        <Link to='/'>Home</Link>
+        <Link to='/timeline'>Timeline</Link>
+      </footer>
     </BrowserRouter>
   </Provider>,
   document.getElementById('app')
